9fac5593f9c512d63001e7ebb1abc244a16ac85f
Will Luft <william.d.luft@gmail.com>
2021-01-17 17:53:29 -0500

    Broken homepage into small components, finished styling menu-items

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../components/directory/Directory';
+import './homepageStyles.scss';
+
+const Homepage = () => (
+  <div clasName="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
